805a530ca9859c9c441544a3cb978b1edd2b246e
samuelnub <notsamnub@gmail.com>
2018-08-04 22:13:19 +0100

    ooof
    test

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ window.onload = () => {
     shake.start();
 
     let sounds = [
-        new Howl({ src: ["./sounds/oofRoblox.wav"] }),
+        new Howl({ src: ["./sounds/oofRoblox.mp3"] }),
         new Howl({ src: ["./sounds/oofSteve.mp3"] })
     ];
+
+    let oofCircleDiv = document.getElementById("oof-circle");
+    let expandTransition = "expand";
 
     window.addEventListener("shake", shakeCallback, false);
     function shakeCallback() {
         sounds[getRandomInt(0, sounds.length-1)].play();
 
-
+        oofCircleDiv.classList.add(expandTransition);
     }
 };
 
